d1f83a2adbbeaf64bfe65aab49704883ed12c63a
Jaime Elcarte <jelcarte@veridas.com>
2024-09-28 20:35:17 +0200

    Add Player components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
