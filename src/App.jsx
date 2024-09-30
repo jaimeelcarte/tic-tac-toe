712f42927364683c856eda50dbd21e12ef3b0cd9
Jaime Elcarte <jelcarte@veridas.com>
2024-09-30 20:56:18 +0200

    Add winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
