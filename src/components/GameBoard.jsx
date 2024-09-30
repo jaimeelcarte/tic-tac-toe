5635c55336df11a649d0d3be7a47016fde0f3d10
Jaime Elcarte <jelcarte@veridas.com>
2024-09-30 20:51:51 +0200

    Disable buttons if already clicked

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,7 +33,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
                   {playerSymbol}
                 </button>
               </li>
