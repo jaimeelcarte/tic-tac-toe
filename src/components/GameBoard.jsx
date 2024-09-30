04514eda5e6f11b84d8e5f36d7245f2071441f9f
Jaime Elcarte <jelcarte@veridas.com>
2024-09-30 20:21:54 +0200

    Implement game board onClick and highlighting

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,37 @@
+import React, { useState } from "react";
+
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-export default function GameBoard() {
-    return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-            <ol>
-                {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
-            </ol>
-        </li>)}
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
+  );
 }
